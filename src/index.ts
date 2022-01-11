490fbaf7fae8ab1d7b7ac4b0e74e03bffdb90832
HassanKhan123 <hk2602809@gmail.com>
2022-01-11 07:41:23 +0500

    fetch method added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({});
-user.set({ name: 'Hassan', age: 21 });
-user.on('change', () => {
-  console.log('change1');
-});
-user.on('change', () => {
-  console.log('change2');
-});
-user.on('change1', () => {
-  console.log('change3');
-});
-
-user.trigger('change');
-
-console.log(user);
+const user = new User({ id: 1 });
+user.fetch();
