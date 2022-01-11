80b60d77cd427015341583894091e30eeff88e24
HassanKhan123 <hk2602809@gmail.com>
2022-01-11 07:14:10 +0500

    trigger method added

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import { User } from './models/User';
 
 const user = new User({});
 user.set({ name: 'Hassan', age: 21 });
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('change1', () => {});
+user.on('change', () => {
+  console.log('change1');
+});
+user.on('change', () => {
+  console.log('change2');
+});
+user.on('change1', () => {
+  console.log('change3');
+});
+
+user.trigger('change');
 
 console.log(user);
