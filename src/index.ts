7670ff0309f6e8266001bf63e215f9ff713f696e
HassanKhan123 <hk2602809@gmail.com>
2022-01-18 11:44:50 +0500

    type guard added

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Lalalal', age: 43 });
+const root = document.getElementById('root');
 
-const userForm = new UserForm(document.getElementById('root'), user);
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,11 +14,20 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
   };
 
   template(): string {
@@ -29,6 +38,7 @@ export class UserForm {
                 <div>User age: ${this.model.get('age')}</div>
 
                 <input />
+                <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
             </div>
         `;
